d347c5a424f21282ebcc4c810efa46402740e9d0
Michał Šrajer <michal@post.pl>
2026-03-20 16:23:31 +0100

    mat: v5

---
+++ mat/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'mat-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './engine.js',
+    './levels.js',
+    './manifest.json',
+    './snd/task-new.mp3',
+    './snd/task-pass.mp3',
+    './snd/task-pass-with-bonus.mp3',
+    './snd/task-fail.mp3',
+    './snd/level-pass.mp3',
+    './snd/level-fail.mp3',
+    './snd/bg.mp3'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
